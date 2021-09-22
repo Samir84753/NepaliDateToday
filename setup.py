f5efd683c0d1aee126d5ea2eb807f81e9b349416
Samir Maharjan <samir84753@gmail.com>
2021-09-22 09:37:33 +0545

    added files

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
     author="Samir Maharjan  ",
     author_email="Samir84753@gmail.com",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
     packages=find_packages(),
     install_requires=['beautifulsoup4==4.10.0','bs4==0.0.1','certifi==2021.5.30','charset-normalizer==2.0.6','idna==3.2','requests==2.26.0','soupsieve==2.2.1','urllib3==1.26.6'],
     keywords=['nepalidate', 'date'],
