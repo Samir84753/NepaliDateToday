a07bc0c38547bf7ecaed52d39de85145234fe3c0
Samir Maharjan <samir84753@gmail.com>
2021-09-22 10:57:51 +0545

    keyword edit

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=['beautifulsoup4==4.10.0','bs4==0.0.1','certifi==2021.5.30','charset-normalizer==2.0.6','idna==3.2','requests==2.26.0','soupsieve==2.2.1','urllib3==1.26.6'],
-    keywords=['nepalidate', 'date'],
+    keywords=['nepalidate', 'date','nepdate'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
