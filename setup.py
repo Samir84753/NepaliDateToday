b61afbc6b55c9a409ff4ff29c2d19ee5c6be7d15
Samir Maharjan <samir84753@gmail.com>
2021-09-22 10:56:25 +0545

    description edit

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 import os
 
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = "Gets today's nepali date"
-LONG_DESCRIPTION = "A package that allows to get today's nepali date."
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
 
 # Setting up
 setup(
@@ -18,6 +20,8 @@ setup(
     author="Samir Maharjan  ",
     author_email="Samir84753@gmail.com",
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=['beautifulsoup4==4.10.0','bs4==0.0.1','certifi==2021.5.30','charset-normalizer==2.0.6','idna==3.2','requests==2.26.0','soupsieve==2.2.1','urllib3==1.26.6'],
     keywords=['nepalidate', 'date'],
